e83844037b04a3168f3616b7d2625508c2bc588f
ZakAleks <admin@ideal-clean.ru>
2019-01-06 14:43:47 +0300

    Revert "Revert "NewTest""
    This reverts commit 5db03fe9820194cd2350c1add9fe6fe9f0df155e.

---
+++ KursUnitTest/KursUnitTest/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace KursUnitTest
+{
+
+    [TestFixture]
+
+    public class FirstKursTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        /// <summary>
+        /// функция выполняется перед каждым тестом
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        /// <summary>
+        /// функция выполняется перед каждым тестом
+        /// </summary>
+        [OneTimeSetUp]
+        public void OnOneTimeSetUp()
+        {
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://www.Google.com";
+            driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            driver.FindElement(By.Name("btnK")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+        }
+
+        /// <summary>
+        /// функция выполняется после каждого теста
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
+        /// <summary>
+        /// функция выполняется после каждого теста
+        /// </summary>
+        [OneTimeTearDown]
+        public void OnOneTimeTearDown()
+        {
+
+        }
+    }
+}
