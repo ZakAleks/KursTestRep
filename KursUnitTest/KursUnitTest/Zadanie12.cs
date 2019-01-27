11e802a9125e18fab969526c5f2ed606913e5544
ZakAleks <admin@ideal-clean.ru>
2019-01-27 21:34:59 +0300

    Update Zadanie12.cs

--- KursUnitTest/KursUnitTest/Zadanie12.cs
+++ KursUnitTest/KursUnitTest/Zadanie12.cs
@@ -29,18 +29,49 @@ namespace KursUnitTest
             return code;
         }
 
+        public class Product
+        {
+            public Product()
+            {
+                Name = "testitem_" + Zadanie12.GetRandomDigit(2);
+                Quantity = "10";
+                Code = "rd" + Zadanie12.GetRandomDigit(4);
+                ImgPath = System.IO.Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+                ImgPath = Directory.GetParent(ImgPath).Parent.FullName;
+                ImgPath = System.IO.Path.Combine(ImgPath, "images\\test_duck.png");
+                Keywords = "Test Keywords";
+                ShortDescription = "Test Short Description";
+                Description = "Test Description";
+                HeadTitle = "Test Head Title";
+
+                MetaDescription = "Test Meta Description";
+                PurchasePrice = "1";
+                PricesUSD = "1";
+                PricesEUR = "1";
+            }
+
+            public string Name { get; set; }
+            public string Quantity { get; set; }
+            public string Code { get; set; }
+            public string ImgPath { get; set; }
+            public string Keywords { get; set; }
+            public string ShortDescription { get; set; }
+            public string Description { get; set; }
+            public string HeadTitle { get; set; }
+
+            public string MetaDescription { get; set; }
+            public string PurchasePrice { get; set; }
+            public string PricesUSD { get; set; }
+            public string PricesEUR { get; set; }
+
+        }
+
+
         [Test]
         public void Test1()
         {
 
-            string Name = "testitem_" + Zadanie12.GetRandomDigit(2);
-            string Quantity = "10";
-            string Code = "rd"+ Zadanie12.GetRandomDigit(4);
-
-            string ImgPath = System.IO.Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
-            ImgPath = Directory.GetParent(ImgPath).Parent.FullName;
-            ImgPath = System.IO.Path.Combine(ImgPath, "images\\test_duck.png");
-
+            var product = new Product();
             //переходим на сайт
             driver.Url = "http://localhost/litecart/admin/login.php";
             var el = driver.FindElement(By.Name("username"));
@@ -53,9 +84,9 @@ namespace KursUnitTest
             var CheckBoxStatus = driver.FindElements(By.CssSelector("input[name='status']"));
             CheckBoxStatus[0].Click();
 
-            driver.FindElement(By.CssSelector("input[name='name[en]']")).SendKeys(Name);
+            driver.FindElement(By.CssSelector("input[name='name[en]']")).SendKeys(product.Name);
 
-            driver.FindElement(By.CssSelector("input[name='code']")).SendKeys(Code);
+            driver.FindElement(By.CssSelector("input[name='code']")).SendKeys(product.Code);
                        
             var CheckRoot = driver.FindElement(By.CssSelector("input[name='categories[]'][data-name='Root']"));
             if (CheckRoot.GetCssValue("checked")!=null)
@@ -72,9 +103,9 @@ namespace KursUnitTest
 
             var Qua = driver.FindElement(By.CssSelector("input[name='quantity']"));
             Qua.Clear();
-            Qua.SendKeys(Quantity);
+            Qua.SendKeys(product.Quantity);
 
-            driver.FindElement(By.CssSelector("input[name='new_images[]']")).SendKeys(ImgPath);
+            driver.FindElement(By.CssSelector("input[name='new_images[]']")).SendKeys(product.ImgPath);
 
             var DataFrom = driver.FindElement(By.CssSelector("input[name='date_valid_from']"));
             DataFrom.SendKeys("27012019");
@@ -88,27 +119,29 @@ namespace KursUnitTest
             var SelectElement = new SelectElement(Manufacture);
             SelectElement.SelectByValue("1");
 
-            driver.FindElement(By.CssSelector("input[name='keywords']")).SendKeys("Keywords");
-            driver.FindElement(By.CssSelector("input[name='short_description[en]']")).SendKeys("Short Description");
-            driver.FindElement(By.CssSelector("div[class='trumbowyg-editor']")).SendKeys("Description");
-            driver.FindElement(By.CssSelector("input[name='head_title[en]']")).SendKeys("Head Title");
-            driver.FindElement(By.CssSelector("input[name='meta_description[en]']")).SendKeys("Meta Description");
+            driver.FindElement(By.CssSelector("input[name='keywords']")).SendKeys(product.Keywords);
+            driver.FindElement(By.CssSelector("input[name='short_description[en]']")).SendKeys(product.ShortDescription);
+            driver.FindElement(By.CssSelector("div[class='trumbowyg-editor']")).SendKeys(product.Description);
+            driver.FindElement(By.CssSelector("input[name='head_title[en]']")).SendKeys(product.HeadTitle);
+            driver.FindElement(By.CssSelector("input[name='meta_description[en]']")).SendKeys(product.MetaDescription);
             driver.FindElement(By.CssSelector("a[href$='#tab-prices']")).Click();
-            driver.FindElement(By.CssSelector("input[name='purchase_price']")).SendKeys("1");
+            var PuPr = driver.FindElement(By.CssSelector("input[name='purchase_price']"));
+            PuPr.Clear();
+            PuPr.SendKeys(product.PurchasePrice);
 
             var PurchaseCurrency = driver.FindElement(By.Name("purchase_price_currency_code"));
             var se3 = new SelectElement(PurchaseCurrency);
             se3.SelectByValue("USD");
 
-            driver.FindElement(By.CssSelector("input[name='prices[USD]']")).SendKeys("1");
+            driver.FindElement(By.CssSelector("input[name='prices[USD]']")).SendKeys(product.PricesUSD);
 
-            driver.FindElement(By.CssSelector("input[name='prices[EUR]']")).SendKeys("1");
+            driver.FindElement(By.CssSelector("input[name='prices[EUR]']")).SendKeys(product.PricesEUR);
 
             driver.FindElement(By.CssSelector("button[name='save']")).Click();
 
             try
             {
-                driver.FindElement(By.XPath("//table[@class='dataTable']//a[contains(.,'" + Name + "')]"));
+                driver.FindElement(By.XPath("//table[@class='dataTable']//a[contains(.,'" + product.Name + "')]"));
               
             }
             catch
